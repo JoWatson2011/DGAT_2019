08221c6f3eb7bf7bca3020d4cdd3604ea9649c63
jowatson2011 <joanne.watson-5@postgrad.manchester.ac.uk>
2019-12-02 15:00:27 +0000

    craigamp figure: selected graph type 2 and removed other code. included chr 23 - 25

--- src/CraigAmp.R
+++ src/CraigAmp.R
@@ -18,17 +18,19 @@ chr18to20 <- amp %>%
                                no = ifelse(chr == 20, paste0("20.", start), NA)
                                )
                         )
-         ) #%>% 
-  mutate(start = as.numeric(start))
+         )
 
+chr23to25 <- amp %>% 
+  filter(chr %in% c(23, 24, 25)) %>% 
+  mutate(start = ifelse(chr == 23, yes = paste0("18.", start), 
+                        no = ifelse(chr == 24, yes = paste0("19.", start), 
+                                    no = ifelse(chr == 25, paste0("20.", start), NA)
+                        )
+  )
+  )
 
-cols <- c("#A33141", "#3B5998")
-
-lapply(cols, function(col){
-  ggplot(chr18to20, aes(x = start, y = g, group = 1)) + 
-    geom_line(color = col) +
-    geom_line(aes(x = 0:(nrow(chr18to20)-1), y= 0), alpha = 0) +
-    geom_ribbon(aes(ymax = g), ymin = 0, color = col, fill = col) +
+ggplot(chr18to20, aes(x = start, y = g, group = 1)) + 
+    geom_line(color = "#d81d1d") + 
     theme(legend.position = "none",
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
@@ -39,52 +41,19 @@ lapply(cols, function(col){
           axis.text = element_blank(),
           text = element_blank()
     )
-  ggsave(paste0("results/CraigAmp/", col, "_1.png"))
+ggsave(paste0("results/CraigAmp/chr18_20_", "#d81d1d", "_2.png"))
+
+ggplot(chr23to25, aes(x = start, y = g, group = 1)) + 
+  geom_line(color = "#d81d1d") + 
+  theme(legend.position = "none",
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        text = element_blank()
+  )
+ggsave(paste0("results/CraigAmp/chr23_25_", "#d81d1d", "_2.png"))
   
-  ggplot(chr18to20, aes(x = start, y = g, group = 1)) + 
-    geom_line(color = col) + 
-    theme(legend.position = "none",
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          panel.background = element_blank(),
-          axis.line = element_blank(),
-          axis.ticks = element_blank(),
-          axis.text = element_blank(),
-          text = element_blank()
-    )
-  ggsave(paste0("results/CraigAmp/", col, "_2.png"))
-  
-  
-  ggplot(chr18to20, aes(x = start, y = g, group = 1)) + 
-    geom_line(color = col) +
-    scale_y_continuous(name = "Amplification G-score") +
-    scale_x_discrete(name = "Chromosome", 
-                     labels = NULL
-    ) +
-    theme(legend.position = "none",
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          panel.background = element_blank(),
-          axis.line = element_line(colour = "grey"),
-          axis.ticks.x = element_blank())
-  ggsave(paste0("results/CraigAmp/", col, "_3.png"))
-  
-  ggplot(chr18to20, aes(x = start, y = g, group = 1)) + 
-    geom_line(color = col) +
-    geom_line(aes(x = 0:(nrow(chr18to20)-1), y= 0), alpha = 0) +
-    geom_ribbon(aes(ymax = g), ymin = 0, color = col, fill = col) +
-    scale_y_continuous(name = "Amplification G-score") +
-    scale_x_discrete(name = "Chromosome", 
-                     labels = NULL
-    ) +
-    theme(legend.position = "none",
-          panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          panel.background = element_blank(),
-          axis.line = element_line(colour = "grey"),
-          axis.ticks.x = element_blank())
-  ggsave(paste0("results/CraigAmp/", col, "_4.png"))
-})
